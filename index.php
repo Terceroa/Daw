22714fbb55b6d7cc6ff966e07f7cf2d90d755e68
Giovanni Vazquez <giovannivazquez@terceroa.com.mx>
2016-07-07 21:29:41 -0500

    Primer Commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+ <meta charset="utf-8">
+ <title>CAMBIAR(TITULO)</title>
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+<header id="Cabecera">
+   <h1 id="Titulo">Desarrollo de Aplicaciones Web</h1>
+</header>
+
+<nav>
+    <ul id="Menu">
+        <li id="MenuLi"> Home </li>
+        <li id="MenuLi"> Blog </li>
+        <li id="MenuLi"> Usuarios </li>
+    </ul>
+</nav>
+
+<section id="Seccion">
+    <h1 id="Texto">Aqui va algo</h1>
+</section>
+
+<aside id="Columna">
+    <h1 id="Texto">Aqui va algo</h1>
+</aside>
+
+
+<footer id="Pie">
+    Derechos Reservados &copy; 2016-2017
+</footer>
+
+</body>
+
+</html>
